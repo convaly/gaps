2376a8da93fd7822f1e6637cb1d8ef51ecd63431
John <michniewicz.john@gmail.com>
2018-07-16 12:51:20 +0200

    Add method for event creation

--- gocal.py
+++ gocal.py
@@ -33,34 +33,34 @@ class GCalendar:
             start = event['start'].get('dateTime', event['start'].get('date'))
             print(start, event['summary'])
 
+    def addEvent(self, name, start, end):
+        event = {
+            'summary': name,
+            #'location': '800 Howard St., San Francisco, CA 94103',
+            #'description': 'A chance to hear more about Google\'s developer products.',
+            'start': {
+                'dateTime': start,
+                #'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+                'dateTime': end,
+                #'timeZone': 'America/Los_Angeles',
+            },
+            # 'recurrence': [
+            #     'RRULE:FREQ=DAILY;COUNT=2'
+            # ],
+            # 'attendees': [
+            #     {'email': 'lpage@example.com'},
+            #     {'email': 'sbrin@example.com'},
+            # ],
+            'reminders': {
+                'useDefault': False,
+                # 'overrides': [
+                #     {'method': 'email', 'minutes': 24 * 60},
+                #     {'method': 'popup', 'minutes': 10},
+                # ],
+            },
+        }
 
-
-# event = {
-#   'summary': 'Google I/O 2015',
-#   #'location': '800 Howard St., San Francisco, CA 94103',
-#   'description': 'A chance to hear more about Google\'s developer products.',
-#   'start': {
-#     'dateTime': '2018-07-28T09:00:00-07:00',
-#     #'timeZone': 'America/Los_Angeles',
-#   },
-#   'end': {
-#     'dateTime': '2018-07-28T17:00:00-07:00',
-#     #'timeZone': 'America/Los_Angeles',
-#   },
-#   # 'recurrence': [
-#   #   'RRULE:FREQ=DAILY;COUNT=2'
-#   # ],
-#   # 'attendees': [
-#   #   {'email': 'lpage@example.com'},
-#   #   {'email': 'sbrin@example.com'},
-#   # ],
-#   'reminders': {
-#     'useDefault': False,
-#     'overrides': [
-#       # {'method': 'email', 'minutes': 24 * 60},
-#       # {'method': 'popup', 'minutes': 10},
-#     ],
-#   },
-# }
-# event = self.service.events().insert(calendarId='primary', body=event).execute()
-# print('Event created: %s' % (event.get('htmlLink')))
+        event = self.service.events().insert(calendarId='primary', body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
--- nona.py
+++ nona.py
@@ -1,6 +1,9 @@
 
-from gocal import *
-
+from gocal import GCalendar
 c=GCalendar()
 c.setup()
 c.callapi()
+a='test'
+s='2018-07-28T09:00:00-07:00'
+e='2018-07-28T17:00:00-07:00'
+c.addEvent(a,s,e)
